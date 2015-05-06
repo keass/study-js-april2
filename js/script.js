55ea5b2cb0b447a9742e2fbc64cfad09a2e26d7c
dongsoo <keass9@gmail.com>
2015-05-06 14:54:35 +0900

    이벤트 고려해서 다시

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
 "use strict";
 
+/*
+* 드래그 앤 드롭 가능한 환경 만들고
+*   - 마우스 캐치를 먼저
+* 드래그 시작
+*   - 마우스 클릭하면 이벤트 시작
+*   - 떼기전에 마우스를 옴기면 오브젝트가 따라붙는다
+*   - 떼면 사라진다
+*   - 끝으로 들어가 떼면 이벤트2
+* 드래그 끝을 정한다
+*   - 드래그가 시작되면 드래그엔드도 대비? 활성?
+*   - 드래그A가 영역내로 와서 떨어지면 이벤트 2
+* */
+
+function Dragenv(a,b){
+    this._init(a,b);
+    this.mouse();
+}
+
+Dragenv.prototype = {
+    _init:function(val1, val2) {
+        this.dragStartItem = document.getElementsByClassName(val1);
+        this.dragEndItem = document.getElementsByClassName(val2);
+    },
+    mouse:function(){
+        //var _self = this;
+        this.dragStartItem[0].addEventListener('mousedown', function(){
+
+            //console.log('오브젝트1 마우스다운');
+        });
+        this.dragEndItem[0].addEventListener('mouseover',function(){
+           console.log('오브젝트2 마우스오버');
+        });
+    }
+};
+/*
+var dragstart = function (){
+    console.log('start');
+};
+
+var dragend = function (){
+    console.log('end');
+};*/
+
+var dragtest = new Dragenv('obj1','obj2');
 
 
+
+/*
 var stage = document.querySelector('.stage');
 var object1 = document.querySelector('.obj1');
 var object2 = document.querySelector('.obj2');
@@ -50,7 +96,7 @@ object2.addEventListener('dragover', function(e) {
     e.dataTransfer.dropEffect = 'move';
 
     return false;
-});
+});*/
 
 /*
 
